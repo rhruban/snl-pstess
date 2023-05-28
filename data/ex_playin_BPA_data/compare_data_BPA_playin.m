2d17abdaf8b7aec25858bf0a05d3a58a5a4fc58a
rhruban <rhruban@gmail.com>
2023-05-28 17:38:16 -0600

    added josh plots to compare_data_BPA_playin

--- data/ex_playin_BPA_data/compare_data_BPA_playin.m
+++ data/ex_playin_BPA_data/compare_data_BPA_playin.m
@@ -8,21 +8,23 @@ clc;
 %% Load data
 pmu = load("d_centralia_play_1223239to1225021.mat"); % generated from PST simulation
 pst = load("playindata_centralia_1223239to1225021.mat"); % playin
-
+jw = load("jw_centralia_play_1223239to1225021_fullfeature.mat"); % Josh's playin
 
 %% plot PMU comparison
 figure();
 subplot(211);
 hold on;
-plot(pmu.t,-pmu.p,'LineWidth',2,'DisplayName','PMU');
+plot(pmu.t,-pmu.p,'k','LineWidth',2,'DisplayName','PMU');
 plot(pst.g.t,pst.g.mac.pelect(1,:),'LineWidth',2,'DisplayName','Play-in');
+plot(jw.t,-jw.Phat0,'LineWidth',2,'DisplayName','Josh Play-in');
 ylabel('P')
 hold off;
 
 subplot(212);
 hold on;
-plot(pmu.t,-pmu.q,'LineWidth',2,'DisplayName','PMU');
+plot(pmu.t,-pmu.q,'k','LineWidth',2,'DisplayName','PMU');
 plot(pst.g.t,pst.g.mac.qelect(1,:),'LineWidth',2,'DisplayName','Play-in');
+plot(jw.t,-jw.Qhat0,'LineWidth',2,'DisplayName','Josh Play-in');
 legend('Location','best');
 ylabel('Q')
 xlabel('time (s)')
@@ -34,6 +36,7 @@ subplot(211);
 hold on;
 plot(pmu.t,abs(pmu.v),'LineWidth',2,'DisplayName','PMU');
 plot(pst.g.t,abs(pst.g.bus.bus_v(1,:)),'LineWidth',2,'DisplayName','Play-in');
+plot(jw.t_save,abs(jw.Vt_save),'LineWidth',2,'DisplayName','Josh Play-in');
 ylabel('V mag (pu)')
 hold off;
 
@@ -41,6 +44,7 @@ subplot(212);
 hold on;
 plot(pmu.t,unwrap(angle(pmu.v)),'LineWidth',2,'DisplayName','PMU');
 plot(pst.g.t,unwrap(angle(pst.g.bus.bus_v(1,:))),'LineWidth',2,'DisplayName','Play-in');
+plot(jw.t_save,unwrap(angle(jw.Vt_save)-angle(jw.Vt_save(1))),'LineWidth',2,'DisplayName','Josh Play-in');
 legend('Location','best');
 xlabel('time (s)')
 ylabel('V angle (rad)')
